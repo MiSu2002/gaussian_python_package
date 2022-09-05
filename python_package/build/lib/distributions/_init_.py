e26949775727fe5c439c17ba69e9032179cba819
Soumita Basu <moon1237879@gmail.com>
2022-09-06 01:18:11 +0530

    Update _init_.py

--- python_package/build/lib/distributions/_init_.py
+++ python_package/build/lib/distributions/_init_.py
@@ -1,4 +1,8 @@
 from gaussian_distribution import Gaussian
+from binomial_distribution import Binomial
 
+#Few examples
 gaussian_one = Gaussian(22, 2)
+binomial_one = Binomial(.4, 20)
 print(gaussian_one.mean)
+print(binomial_one)
